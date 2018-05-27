f7d2fa22e355b1151106412b16d8b3ca117f9c28
DiaKnowledge <ogasawara.no.nexus7@gmail.com>
2018-05-27 23:36:17 +0900

    ツール設計見直し

--- src/txt2html.rb
+++ src/txt2html.rb
@@ -2,64 +2,112 @@
 # 技術士回答HTMLを出力するスクリプト
 # 原稿用紙は24*25
 
+class GenkoString < String
+  def linesize
+    if self.size % 24 == 0
+      linesize = self.size / 24
+    else
+      linesize = (self.size / 24) + 1
+    end
+    linesize
+  end
+end
+
+class HTMLArray < Array
+  def create_html
+    # HTML出力
+    html_outputer = HTMLOutputer.new
+    html_outputer.start_genko
+    html_outputer.start_paragraph
+
+    paragraph_flag = true
+    line_no = 0
+    self.each do |item|
+      # 空行 -> パラグラフ変更
+      if item == ""
+        paragraph_flag = true
+        html_outputer.end_paragraph
+        html_outputer.start_paragraph
+        next
+      end
+
+      # 行数取得。25行を超える場合、改ページ
+      if line_no + item.linesize > 25
+        html_outputer.end_paragraph
+        html_outputer.end_genko
+        puts "(#{line_no}/25)行"
+        html_outputer.separate
+        html_outputer.start_genko
+        html_outputer.start_paragraph
+        line_no = 0
+      end
+      line_no = line_no + item.linesize
+
+      if paragraph_flag == true
+        # パラグラフ変更直後->下線を引く
+        html_outputer.title(item)
+      elsif item[0] == "・"
+        # 箇条書き->普通に出力
+        html_outputer.list(item)
+      else
+        # その他->先頭にスペース付けて、普通に出力
+        html_outputer.str(item)
+      end
+
+      paragraph_flag = false
+    end
+    html_outputer.end_paragraph
+    html_outputer.end_genko
+    puts "(#{line_no}/25)行"
+  end
+end
+
+class HTMLOutputer
+  def initialize
+  end
+  def start_genko
+    puts "<div class=\"sakubun\">"
+  end
+  def start_paragraph
+    puts "<p>"
+  end
+  def end_genko
+    puts "</div>"
+  end
+  def end_paragraph
+    puts "</p>"
+  end
+  def separate
+    puts "<hr />"
+  end
+  def title(item)
+    puts "<u>#{item}</u><br />"
+  end
+  def list(item)
+    puts "#{item}<br />"
+  end
+  def str(item)
+    puts "　#{item}<br />"
+  end
+end
+
 # 引数チェック
 if ARGV[0] == nil
   raise "ARGS ERROR: ARGV[0] -> csv file name"
 end
 
 # txtファイル読み込み
-str_ary = []
+ary = HTMLArray.new
 open(ARGV[0]) do |file|
   file.each do |line|
-    str_ary << line.chomp
+    # 先頭#はコメントとみなす
+    if line[0] == "#"
+      next
+    end
+    ary << GenkoString.new(line.chomp)
   end
 end
 
-# HTML出力
-puts "<div class=\"sakubun\">"
-puts "<p>"
+# HTML文字列出力
+ary.create_html
 
-paragraph_flag = true
-line_no = 0
-str_ary.each do |item|
-  # 空行 -> パラグラフ変更
-  if item == ""
-    paragraph_flag = true
-    puts "</p>"
-    puts "<p>"
-    next
-  end
-
-  # 行数取得。25行を超える場合、改ページ
-  if item.size % 24 == 0
-    linesize = item.size / 24
-  else
-    linesize = (item.size / 24) + 1
-  end
-  if line_no + linesize > 25
-    puts "</p>"
-    puts "</div>"
-    puts "(#{line_no}/25)行"
-    puts "<hr />"
-    puts "<div class=\"sakubun\">"
-    puts "<p>"
-    line_no = 0
-  end
-  line_no = line_no + linesize
-
-  if paragraph_flag == true
-    # パラグラフ変更直後->下線を引く
-    puts "<u>#{item}</u><br />"
-  elsif item[0] == "・"
-    # その他->普通に出力
-    puts "#{item}<br />"
-  else
-    # その他->普通に出力
-    puts "　#{item}<br />"
-  end
-
-  paragraph_flag = false
-end
-puts "</p>"
-puts "</div>"
-puts "(#{line_no}/25)行"
