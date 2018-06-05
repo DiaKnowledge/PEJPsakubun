af2bb62ce51138bbf8e26137d2bf74889847c36a
DiaKnowledge <ogasawara.no.nexus7@gmail.com>
2018-06-05 23:19:12 +0900

    テキスト整形スクリプト作成

---
+++ create_txt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 引数チェック
+if [ $# -ne 1 ]; then
+  echo "指定された引数は${#}個です。"
+  echo "第一引数に過去問のファイル名を入力してください。"
+  exit 1
+fi
+
+ruby src/txt2html.rb data/$1 txt/$1
+
+if [ $? -eq 0 ]; then
+  echo "txt/$1.txtを正常に作成完了しました。"
+else
+  echo "[ERROR] txt/$1.txt作成に失敗しました。"
+  exit 1
+fi
+
---
+++ src/txt2txt.rb
@@ -0,0 +1,52 @@
+# 原稿用紙に記載する文字列を格納するクラス
+class GenkoString < String
+  COLUMN = 24
+
+  def initialize(line)
+    if line[0] != "・" and line[0] != "（"
+      line = "　" + line
+    end
+    super
+  end
+
+  def linesize
+    if (size % COLUMN).zero?
+      linesize = size / COLUMN
+    else
+      linesize = (size / COLUMN) + 1
+    end
+    linesize
+  end
+
+  def getline(no)
+    self[(no - 1)*COLUMN, COLUMN]
+  end
+end
+
+# 引数チェック
+if ARGV[1].nil?
+  raise "ARGS ERROR: ARGV[0]->input_base_txt ARGV[1]->output_txt"
+end
+
+open(ARGV[0]) do |input_file|
+  open(ARGV[1], "w") do |output_file|
+    output_file.puts "　１２３４５６７８９０１２３４５６７８９０１２３４"
+    line_no = 1
+    input_file.each do |line|
+      # 先頭#はコメントとみなす
+      if line[0] == "#"
+        next
+      end
+      # 空行も飛ばす
+      if line[0] == "\n"
+        next
+      end
+      genko_line = GenkoString.new(line.chomp)
+      for i in 1..(genko_line.linesize)
+        output_file.puts "%02d"%[line_no] + genko_line.getline(i)
+        line_no += 1
+      end
+    end
+  end
+end
+
